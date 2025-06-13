3dce583c7a57a47449fa07fa2b16b6cf5da925ba
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-13 18:35:16 +0200

    init tmt script

---
+++ tmt.py
@@ -0,0 +1,19 @@
+
+def __get_uncharged_mass_from_seq(seq: str) -> float:
+    return
+
+
+def __get_uncharged_mass_from_exp_mass(mass: float, charge: int) -> float:
+    return
+
+
+def __get_settings(toml: str) -> dict:
+    return
+
+
+def __get_tmt_intensities(spectrum: dict) -> dict:
+    return
+
+
+def main(argv = None) -> None:
+    return
