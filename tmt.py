37dc57c41946b622c03fba92499d76cbac2e45ac
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-06-14 23:22:14 +0200

    uncharged mass calculation

--- tmt.py
+++ tmt.py
@@ -1,10 +1,12 @@
+PROTON = 1.007276466812
+
 
 def __get_uncharged_mass_from_seq(seq: str) -> float:
     return
 
 
-def __get_uncharged_mass_from_exp_mass(mass: float, charge: int) -> float:
-    return
+def __get_uncharged_mass_from_exp_mass(mz: float, charge: int) -> float:
+    return mz * charge - proton * charge
 
 
 def __get_settings(toml: str) -> dict:
