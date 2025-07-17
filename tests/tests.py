fb3545dcdaef2fe0ac00d32e4dba6ef8ae9f35f2
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-07-17 14:26:20 +0200

    F401

--- tests/tests.py
+++ tests/tests.py
@@ -7,14 +7,14 @@
 
 
 def test1():
-    from tmt_chimerys import main
+    from tmt_chimerys import main  # noqa: F401
     from tmt_chimerys import __version
 
     assert int(__version.split(".")[2]) >= 9
 
 
 def test2():
-    from tmt_spectronaut import main
+    from tmt_spectronaut import main  # noqa: F401
     from tmt_spectronaut import __version
 
     assert int(__version.split(".")[2]) >= 9
