dce67b5449835b62fbd016a69a2bea8656e8d450
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-08-22 12:55:05 +0200

    update diann script to 1.0.0
    - use openms quantification
    - use resolution

--- tmt_diann.py
+++ tmt_diann.py
@@ -18,18 +18,25 @@ import argparse
 import pandas as pd
 from tqdm import tqdm
 
+from typing import Optional
 from typing import Dict
 from typing import Any
 
 from tmt_chimerys import TMT
+from tmt_chimerys import RESOLUTION_GUI_COLS
 from tmt_chimerys import __get_bool_from_value
 from tmt_chimerys import __read_settings
 from tmt_chimerys import __get_tmt_intensities
+from tmt_chimerys import __get_tmt_intensities_oms
+from tmt_chimerys import __get_consensusXML_df
+from tmt_chimerys import __get_consensusXML_map
+from tmt_chimerys import __get_resolution_gui_map
+from tmt_chimerys import __get_resolution_gui_values
 from tmt_chimerys import __get_windows
 from tmt_spectronaut import __read_spectra
 from tmt_spectronaut import __get_ms2_spectrum
 
-__version = "0.0.10"
+__version = "1.0.0"
 __date = "2025-07-22"
 
 
@@ -37,14 +44,18 @@ __date = "2025-07-22"
 # currently based on the report parquet
 def __annotate_diann_result(
     diann_filename: str,
+    spectrum_filename: str,
     spectra: Dict[str, Any],
     settings: Dict[str, Any],
+    consensusXML_map: Optional[Dict[int, Dict[int, pd.Series]]] = None,
+    resolution_gui_map: Optional[Dict[str, Dict[int, pd.Series]]] = None,
     verbose: int = 2,
 ) -> pd.DataFrame:
     # spectra should be given by __read_spectra
     # settings should be given by __read_settings
     df = pd.read_parquet(diann_filename)
     channels = {key: [] for key in TMT.keys()}
+    resolution = {f"RESGUI_{key}": [] for key in RESOLUTION_GUI_COLS}
     purities = list()
     parsed_scannr = list()
     nr_of_missing_ms1 = 0
@@ -98,16 +109,32 @@ def __annotate_diann_result(
         purity = spectrum_purity["purity"]
         # if a spectrum is found -> purity and quantify
         if spectrum is not None:
-            tmt_quants = __get_tmt_intensities(spectrum)
+            if consensusXML_map is None:
+                tmt_quants = __get_tmt_intensities(spectrum)
+            else:
+                tmt_quants = __get_tmt_intensities_oms(spectrum, consensusXML_map)
             for key in channels.keys():
                 channels[key].append(tmt_quants[key])
+            if resolution_gui_map is None:
+                for key in resolution.keys():
+                    resolution[key].append(None)
+            else:
+                resolution_values = __get_resolution_gui_values(
+                    spectrum, resolution_gui_map, spectrum_filename
+                )
+                for key in resolution.keys():
+                    resolution[key].append(resolution_values[key])
             purities.append(purity)
-            if purity < filter_threshold:
+            if purity is None:
+                nr_of_missing_ms1 += 1
+            if purity is not None and purity < filter_threshold:
                 nr_of_impure_ids += 1
             parsed_scannr.append(spectrum["scan_nr"])
         else:
             for key in channels.keys():
                 channels[key].append(None)
+            for key in resolution.keys():
+                resolution[key].append(None)
             purities.append(None)
             nr_of_missing_ms1 += 1
             parsed_scannr.append(None)
@@ -116,6 +143,9 @@ def __annotate_diann_result(
     df["Parsed MS2 Scan Number"] = parsed_scannr
     for key in channels.keys():
         df[f"Annotated {key}"] = channels[key]
+    if resolution_gui_map is not None:
+        for key in resolution:
+            df[key] = resolution[key]
     print(f"Total number of identifications: {df.shape[0]}")
     print(f"Total number of identifications with impure precursors: {nr_of_impure_ids}")
     print(
@@ -155,12 +185,29 @@ def main(argv=None) -> pd.DataFrame:
         type=str,
     )
     parser.add_argument(
+        "-r",
+        "--resolution",
+        dest="resolution",
+        required=False,
+        default=None,
+        help="Path/name of the resolution.csv file from the Resolution GUI file.",
+        type=str,
+    )
+    parser.add_argument(
         "-w",
         "--window",
         dest="window",
         default=None,
         help="Window size, overrides config file!",
         type=float,
+    )
+    parser.add_argument(
+        "-n",
+        "--native",
+        dest="native",
+        default=False,
+        action="store_true",
+        help="Use native quantification instead of OpenMS quantification which is used by default.",
     )
     parser.add_argument(
         "-v",
@@ -177,8 +224,25 @@ def main(argv=None) -> pd.DataFrame:
         settings["window_size"] = float(args.window)
     print(settings)
     spectra = __read_spectra(args.spectra)
-    df = __annotate_diann_result(args.diann, spectra, settings, int(args.verbose))
-    df.to_parquet(args.diann.split(".parquet")[0] + "_purity_tmt_quant.parquet")
+    consensusXML_map = None
+    if not args.native:
+        consensusXML_df = __get_consensusXML_df(args.spectra)
+        consensusXML_map = __get_consensusXML_map(consensusXML_df)
+    resolution_gui_map = None
+    if args.resolution is not None:
+        resolution_gui_map = __get_resolution_gui_map(args.resolution)
+    df = __annotate_diann_result(
+        diann_filename=args.diann,
+        spectrum_filename=args.spectra,
+        spectra=spectra,
+        settings=settings,
+        consensusXML_map=consensusXML_map,
+        resolution_gui_map=resolution_gui_map,
+        verbose=int(args.verbose),
+    )
+    df.to_parquet(
+        args.diann.split(".parquet")[0] + "_purity_tmt_quant.parquet", index=False
+    )
     return df
 
 
